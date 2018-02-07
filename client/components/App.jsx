96b415deea41e822e503cc53e394fe5c5038b877
Tian <Tian@Louises-iMac-2.local>
2018-02-07 22:46:02 +1300

    set up from in App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,12 +6,30 @@ import NewWorld from './NewWorld'
 class App extends React.Component {
   constructor () {
     super ()
-    this.state = {}
+    this.state = {
+      shoppingList: []
+    }
+    this.addItem=this.addItem.bind(this)
+  }
+  addItem(evt){
+    let list = []
+    list.push(evt.target.value)
+    console.log(list)
   }
   render () {
     return (
       <div>
         <h1>The app is here</h1>
+        <div>
+        <form>
+          <input name='itemOne' /> <br/>
+          <input name='itemTwo' /> <br/>
+          <input name='itemThree' /> <br/>
+          <input name='itemFour' /> <br/>
+          <input name='itemFive' /> <br/>
+          <button type = 'button' onClick={this.addItem}>Press me</button>
+        </form>
+        </div>
         <Countdown />
         <NewWorld />
       </div>
