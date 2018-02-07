a49db59a46fbc61376aa5fb3940f400391ce899c
Tian <Tian@Louises-iMac-2.local>
2018-02-07 22:07:52 +1300

    fixed spelling error in gitignore and package.json. server running

---
+++ client/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor () {
+    super ()
+    this.state = {}
+  }
+  render () {
+    return (
+      <div>
+        <h1>The app is here</h1>
+      </div>
+    )
+  }
+}
+
+
+export default App
